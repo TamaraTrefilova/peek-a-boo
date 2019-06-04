8c14e6a869b30c81ad33c11250d5cfac4e175d13
Rochelle Roberts and Tamara Trefilova <rochelle@>
2019-06-04 11:11:49 -0700

    added fadein and fadeout functions

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,19 @@ $(function() {
     $("#initially-showing").toggle();
     $("#initially-hidden").toggle();
   });
+
+  $(".clickable").click(function() {
+    $("#initially-hidden").fadeIn();
+  });
+
+  $(".clickable").click(function() {
+    $("#initially-showing").fadeOut(5000);
+  });
+
+  $(".new_clickable").click(function() {
+    $("#initially-faddedOut").fadeToggle();
+  });
+  // $(".new_clickable").click(function() {
+  //   $("#initially-faddedIn").fadeIn();
+  // });
 });
